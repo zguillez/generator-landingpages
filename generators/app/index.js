352b6463f8bce06ea3fc5b27a5086ed50bb5e5bb
Guillermo de la Iglesia <zguillez@gmail.com>
2018-01-26 23:03:37 +0100

    fix

--- generators/app/index.js
+++ generators/app/index.js
@@ -25,9 +25,9 @@ module.exports = class extends Generator {
     this.fs.copy(this.templatePath('bin'), this.destinationPath('bin'));
     this.fs.copy(this.templatePath('dist'), this.destinationPath('dist'));
     this.fs.copy(this.templatePath('src'), this.destinationPath('src'));
-    this.fs.copy(this.templatePath('sshconfig'), this.destinationPath('.sshconfig'));
     this.fs.copy(this.templatePath('eslintrc.js'), this.destinationPath('.eslintrc.js'));
     this.fs.copy(this.templatePath(`package.json`), this.destinationPath('package.json'));
+    this.fs.copy(this.templatePath('sshconfig'), this.destinationPath('.sshconfig'));
   }
 
   install() {
